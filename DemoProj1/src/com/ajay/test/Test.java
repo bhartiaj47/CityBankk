19ad1939868164ef4de861ecb0de89a4d6bf902b
Dev1 <Dev1@gmail.com>
2024-08-25 13:32:13 +0530

    tl modified test

--- DemoProj1/src/com/ajay/test/Test.java
+++ DemoProj1/src/com/ajay/test/Test.java
@@ -1,5 +1,9 @@
 package com.ajay.test;
-
+import com.ajay.bean.*;
 public class Test {
+	public static void main(String[] args) {
+		Utility u=new Utility();
+		System.out.println(u.getSysDate());
+	}
 
 }
