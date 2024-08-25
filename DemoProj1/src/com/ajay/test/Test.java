e3ebf400a18f1310f19e208d7a3a2a3167ed6c1c
Dev1 <Dev1@gmail.com>
2024-08-25 13:46:13 +0530

    test deleted by dev1

--- DemoProj1/src/com/ajay/test/Test.java
+++
@@ -1,9 +0,0 @@
-package com.ajay.test;
-import com.ajay.bean.*;
-public class Test {
-	public static void main(String[] args) {
-		Utility u=new Utility();
-		System.out.println(u.getSysDate());
-	}
-
-}
